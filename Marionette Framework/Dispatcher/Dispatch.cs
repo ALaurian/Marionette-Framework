ffc6f1a3f7143c30f381e0dd6d99e3b0fb6b97a1
Avrigeanu Laurian <avrigeanulaurianus@yahoo.ro>
2023-04-14 20:33:41 +0300

    Fixed a bug in the Dispatch.cs

--- Marionette Framework/Dispatcher/Dispatch.cs
+++ Marionette Framework/Dispatcher/Dispatch.cs
@@ -10,6 +10,11 @@ partial class Framework
     //This method Dispatches data from a DT into the Queue using a Dispatcher instance.
     public void Dispatch(string in_FrameworkSettingsPath)
     {
+        //Here we add the dispatcherInput
+        //Write logic to add the DataTable, either from Excel, a Json file or a text file..
+        var dataTable = new DataTable();
+        _dispatcherInput = dataTable;
+        
         //Create a new instance of the Dispatcher class with the given parameters.
         var dispatcher = new Dispatcher(
             _dispatcherInput,
@@ -18,11 +23,6 @@ partial class Framework
             int.Parse(Config["MaxRetryNumber"].ToString()),
             in_FrameworkSettingsPath);
 
-        //Here we add the dispatcherInput
-        //Write logic to add the DataTable, either from Excel, a Json file or a text file..
-        var dataTable = new DataTable();
 
-        _dispatcherInput = dataTable;
-        OrchestratorConnection.ClearQueue(Config["OrchestratorQueueName"].ToString());
     }
 }
